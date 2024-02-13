092c380e2c609edff05c2a4d9a64704b69deed14
schaefers13 <159864867+schaefers13@users.noreply.github.com>
2024-02-13 00:34:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,27 +30,27 @@ function App() {
   },
   {
     id: 3,
-    description: "No tengas miedo, será genial.",
+    description: "No tengas miedo, será genial Jereyla.",
     img: "https://i.pinimg.com/originals/e1/c3/88/e1c388133e0f998e25bb17c837b74a14.gif"
   },
   {
     id: 4,
-    description: "Confía en mí, será divertido.",
+    description: "Confía en mí, será divertido Yeye.",
     img: "https://media.tenor.com/Bn88VELdNI8AAAAi/peach-goma.gif"
   },
   {
     id: 5,
-    description: "No tengas dudas, te hará sonreír.",
+    description: "No tengas dudas, te hará sonreír amor.",
     img: "https://i.pinimg.com/originals/c6/b3/0d/c6b30d1a2dc178aeb92de63295d4ae64.gif"
   },
   {
     id: 6,
-    description: "Te prometo que será inolvidable.",
+    description: "Te prometo que será inolvidable Jere.",
     img: "https://media.tenor.com/N2oqtqaB_G0AAAAi/peach-goma.gif"
   },
   {
     id: 7,
-    description: "No dejes que el miedo te detenga.",
+    description: "No dejes que el miedo te detenga señorita schmidt.",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
@@ -60,12 +60,12 @@ function App() {
   },
   {
     id: 9,
-    description: "Confía en mí.",
+    description: "Confía en mí Yeye.",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 10,
-    description: "No te arrepentirás.",
+    description: "No te arrepentirás bby.",
     img: "https://media.tenor.com/I7KdFaMzUq4AAAAi/peach-goma.gif"
   }]
 
@@ -89,7 +89,7 @@ function App() {
       {
         !valueSi ? (
           <div className="p-5">
-            <h1 className="text-white font-bold text-5xl text-center">¿Quieres ser mi San Valentin?</h1>
+            <h1 className="text-white font-bold text-5xl text-center">¿Quieres ser mi San Valentin Jereyla Schmidt?</h1>
             <img src={Object.keys(randomValor).length === 0 ?
               "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif" : randomValor.img} alt="San Valentin" className="mx-auto" width={400} height={400} />
             <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5 items-center">
@@ -111,13 +111,13 @@ function App() {
                 disabled={imagenCargada} // Deshabilita el botón si la imagen no se ha cargado
               >
                 {Object.keys(randomValor).length === 0 ? "No" : randomValor.description}
-                <span hidden>{document.title = Object.keys(randomValor).length === 0 ? "¿Quieres ser mi San Valentin Yeye?" : randomValor.description}</span>
+                <span hidden>{document.title = Object.keys(randomValor).length === 0 ? "¿Quieres ser mi San Valentin Jereyla Schmidt?" : randomValor.description}</span>
               </button>
             </div>
           </div>
         ) : (
           <div className="flex justify-center items-center flex-col space-y-10">
-            <h1 className="text-4xl text-white font-bold">Sabia que dirias que si ❤️!</h1>
+            <h1 className="text-4xl text-white font-bold">Sabia que dirias que si Jere ❤️!</h1>
             <img src="https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif" alt="" className="mx-auto" />
             <span hidden>{document.title = 'Sabia que dirias que si ❤️!'}</span>
           </div>
